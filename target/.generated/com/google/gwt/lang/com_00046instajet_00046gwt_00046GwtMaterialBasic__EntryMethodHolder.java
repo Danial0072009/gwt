9bdc8ea55d2cbc46ee002c2fad684ff53d578479
Alexander Ulrich <alexanderulrich@gmail.com>
2018-03-09 12:23:51 +0100

    "Unnötige Dateien von Remote löschen"

--- target/.generated/com/google/gwt/lang/com_00046instajet_00046gwt_00046GwtMaterialBasic__EntryMethodHolder.java
+++
@@ -1,6 +0,0 @@
-package com.google.gwt.lang;
-public class com_00046instajet_00046gwt_00046GwtMaterialBasic__EntryMethodHolder {
-  public static final void init() {
-    // to be synthesized later
-  }
-}
